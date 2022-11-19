1c147047ea50d506dffb54397a10f853466c578a
sh0rez <me@shorez.de>
2022-11-19 20:34:58 +0100

    armv4t support

--- build.rs
+++ build.rs
@@ -23,7 +23,8 @@ fn main() {
 
 fn target_has_atomic_cas(target: &str) -> bool {
     match target {
-        "thumbv6m-none-eabi"
+        "armv4t-none-eabi"
+        | "thumbv6m-none-eabi"
         | "msp430-none-elf"
         | "riscv32i-unknown-none-elf"
         | "riscv32imc-unknown-none-elf" => false,
@@ -33,7 +34,8 @@ fn target_has_atomic_cas(target: &str) -> bool {
 
 fn target_has_atomics(target: &str) -> bool {
     match target {
-        "thumbv4t-none-eabi"
+        "armv4t-none-eabi"
+        | "thumbv4t-none-eabi"
         | "msp430-none-elf"
         | "riscv32i-unknown-none-elf"
         | "riscv32imc-unknown-none-elf" => false,
